6a4874a3239889f6484a0aadc46214329c39dad7
wakabau@outlook.com <wakaba.uno@ibm.com>
2021-03-08 11:20:07 +0900

    first commit

--- src/object-detection-video/ObjectDetectionVideo.js
+++ src/object-detection-video/ObjectDetectionVideo.js
@@ -14,55 +14,55 @@ const ObjectDetectionVideo = React.memo(
     })
 
     const detectFrame = useCallback(async () => {
-      const predictions = await model.detect(videoRef.current)
-      if (onPrediction) {
-        onPrediction(predictions)
-      }
-
-      const wantedWidth = videoRef.current.offsetWidth
-      const wantedHeight = videoRef.current.offsetHeight
-      const videoWidth = videoRef.current.videoWidth
-      const videoHeight = videoRef.current.videoHeight
-
-      const scaleX = wantedWidth / videoWidth
-      const scaleY = wantedHeight / videoHeight
-
-      let scale
-      if (fit === 'aspectFit') {
-        scale = Math.min(scaleX, scaleY)
-      } else {
-        scale = Math.max(scaleX, scaleY)
-      }
-
-      const xOffset = (wantedWidth - videoWidth * scale) / 2
-      const yOffset = (wantedHeight - videoHeight * scale) / 2
-
-      const ctx = getRetinaContext(canvasRef.current)
-
-      ctx.setWidth(wantedWidth)
-      ctx.setHeight(wantedHeight)
-      ctx.clearAll()
-
-      // Update predictions to match canvas.
-      const offsetPredictions = predictions.map((prediction) => {
-        let x = prediction.bbox[0] * scale + xOffset
-        const y = prediction.bbox[1] * scale + yOffset
-        const width = prediction.bbox[2] * scale
-        const height = prediction.bbox[3] * scale
-
-        if (mirrored) {
-          x = wantedWidth - x - width
+        const predictions = await model.detect(videoRef.current)
+        if (onPrediction) {
+          onPrediction(predictions)
         }
-        return { ...prediction, bbox: [x, y, width, height] }
-      })
 
-      const renderFunction = render || renderPredictions
+        const wantedWidth = videoRef.current.offsetWidth
+        const wantedHeight = videoRef.current.offsetHeight
+        const videoWidth = videoRef.current.videoWidth
+        const videoHeight = videoRef.current.videoHeight
 
-      renderFunction(ctx, offsetPredictions)
-      requestAnimationFrame(() => {
-        detectFrame()
-      })
-    }, [fit, mirrored, model, onPrediction, render])
+        const scaleX = wantedWidth / videoWidth
+        const scaleY = wantedHeight / videoHeight
+
+        let scale
+        if (fit === 'aspectFit') {
+          scale = Math.min(scaleX, scaleY)
+        } else {
+          scale = Math.max(scaleX, scaleY)
+        }
+
+        const xOffset = (wantedWidth - videoWidth * scale) / 2
+        const yOffset = (wantedHeight - videoHeight * scale) / 2
+
+        const ctx = getRetinaContext(canvasRef.current)
+
+        ctx.setWidth(wantedWidth)
+        ctx.setHeight(wantedHeight)
+        ctx.clearAll()
+
+        // Update predictions to match canvas.
+        const offsetPredictions = predictions.map((prediction) => {
+          let x = prediction.bbox[0] * scale + xOffset
+          const y = prediction.bbox[1] * scale + yOffset
+          const width = prediction.bbox[2] * scale
+          const height = prediction.bbox[3] * scale
+
+          if (mirrored) {
+            x = wantedWidth - x - width
+          }
+          return { ...prediction, bbox: [x, y, width, height] }
+        })
+
+        const renderFunction = render || renderPredictions
+
+        renderFunction(ctx, offsetPredictions)
+        requestAnimationFrame(() => {
+          detectFrame()
+        })
+      }, [fit, mirrored, model, onPrediction, render])
 
     if (canvasRef.current) {
       canvasRef.current.style.position = 'absolute'
